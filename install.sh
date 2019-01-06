73151700fc2323004928aaed37bbb7b01c7bdd7d
rwithik <rwithik@gmail.com>
2019-01-06 17:24:12 +0530

    Scheduled push

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ cp -v ./.config/rofi/config ~/.config/rofi/config
 cp -v ./.config/i3/config ~/.config/i3/config
 cp -v ./.config/ranger/ ~/.config/ranger/*
 cp -v ./.config/polybar/ ~/.config/polybar/*
-cp -v -r ./Scripts/ ~/Desktop/lockscreen.sh
-cp -v -r ./Scripts/ ~/Desktop/wallpaperScript.sh
+cp -v -r ./Scripts/* ~/Desktop/Scripts/
 #cp -r ./zsh-themes ~/.oh-my-zsh/themes
 cp -v ./.Xresources ~/.Xresources
 cp -v ./.zshrc ~/.zshrc
